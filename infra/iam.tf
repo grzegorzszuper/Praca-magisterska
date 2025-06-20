282d259645ce1e4507fb5c5b666b2c8fcb40e36a
Grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-20 22:53:38 +0200

    Update iam.tf

--- infra/iam.tf
+++ infra/iam.tf
@@ -14,6 +14,12 @@ resource "aws_iam_role_policy" "ec2_policy" {
   policy = data.aws_iam_policy_document.ec2_policy.json
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
+
 # 3. Profile dla instancji EC2
 resource "aws_iam_instance_profile" "ec2_profile" {
   name = "thesis-ec2-profile"
