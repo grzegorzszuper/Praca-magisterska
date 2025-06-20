a644443fe2544c27c7a9164635c2b77c0cc06be4
Grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-21 00:59:16 +0200

    Update native.tf

--- infra/native.tf
+++ infra/native.tf
@@ -10,12 +10,40 @@ data "aws_ami" "amazonlinux2" {
   }
 }
 
-# 2. Definicja instancji EC2 dla testu native
+
 resource "aws_instance" "test_native" {
   ami                    = data.aws_ami.amazonlinux2.id
   instance_type          = var.instance_type
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  user_data              = file("${path.module}/user_data_native.tpl")
+ # user_data              = file("${path.module}/user_data_native.tpl")
+
+
+  # --- Dodajemy połączenie przez SSM ---
+  connection {
+    type = "session_manager"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      # Instalacja potrzebnych narzędzi
+      "sudo yum -y install python3 python3-pip git awscli amazon-ssm-agent",
+      "sudo pip3 install numpy psutil",
+
+      # Klon repo i uruchomienie benchmarku
+      "git clone https://github.com/grzegorzszuper/Praca-magisterska.git /opt/repo",
+      "cd /opt/repo/tests",
+      "python3 benchmark.py --size 100 | sudo tee -a /var/log/matrix_native.log",
+      "python3 benchmark.py --size 500 | sudo tee -a /var/log/matrix_native.log",
+      "python3 benchmark.py --size 1000 | sudo tee -a /var/log/matrix_native.log",
+
+      # Eksport do S3
+      "sudo aws s3 cp /var/log/matrix_native.log s3://thesis-logs-dev/native/ --region eu-west-3",
+
+      # (opcjonalnie) shutdown
+      "sudo shutdown -h now",
+    ]
+  }
+
   tags = {
     Name    = "thesis-native"
     Variant = "native"
