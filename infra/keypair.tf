12777380eb711e66218c8ea7a749ef52b5bb443f
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-23 22:12:13 +0200

    dodajemy key-pair na githuba zamiast lokalnie

--- infra/keypair.tf
+++ infra/keypair.tf
@@ -1,8 +1,1 @@
-resource "aws_key_pair" "thesis_key" {
-  key_name   = "thesis-key"
-  public_key = file(var.ssh_public_key_path)
-
-  tags = {
-    Name = "thesis-key"
-  }
-}
+public_key = file("${path.module}/${var.ssh_public_key_path}")
