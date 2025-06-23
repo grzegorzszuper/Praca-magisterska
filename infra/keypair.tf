9d779f80770898c05ea8fb2d5e61ab41849ca047
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-23 22:17:57 +0200

    Update keypair.tf

--- infra/keypair.tf
+++ infra/keypair.tf
@@ -1,1 +1,8 @@
-public_key = file("${path.module}/${var.ssh_public_key_path}")
+resource "aws_key_pair" "thesis_key" {
+  key_name   = "thesis-key"
+  public_key = file("${path.module}/${var.ssh_public_key_path}")
+
+  tags = {
+    Name = "thesis-key"
+  }
+}
