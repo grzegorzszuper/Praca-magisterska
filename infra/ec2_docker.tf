b28dd6d52924adefde9372026ef1019e02956f5c
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-26 17:25:28 +0200

    porpawiony plik docker.tf

--- infra/ec2_docker.tf
+++ infra/ec2_docker.tf
@@ -4,18 +4,15 @@ resource "aws_instance" "test_docker" {
   ami                    = data.aws_ami.amazonlinux2.id
   instance_type          = var.instance_type
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  key_name               = aws_key_pair.generated_key.key_name
-  subnet_id              = var.subnet_id
-  vpc_security_group_ids = [aws_security_group.instance_sg.id]
+  key_name               = aws_key_pair.thesis_key.key_name        # Poprawka
+  vpc_security_group_ids = [aws_security_group.thesis_sg.id]       # Poprawka
+  subnet_id              = data.aws_subnet.default.id              # Dodaj jak w native.tf
 
   tags = {
     Name = "thesis-docker-${var.env}"
   }
-
-  lifecycle {
-    create_before_destroy = true
-  }
 }
+
 
 output "docker_instance_ip" {
   value = aws_instance.test_docker.public_ip
