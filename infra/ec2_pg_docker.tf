9f27f12efab8b3809bc3214efa2a5f59b7de2e96
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-28 20:15:49 +0200

    poprawka ami

--- infra/ec2_pg_docker.tf
+++ infra/ec2_pg_docker.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "pg_docker" {
-  ami = "0359e47f84edf87e7"  # CentOS 7 x86_64 EBS HVM
+  ami = "ami-0359e47f84edf87e7"  # CentOS 7 x86_64 EBS HVM
   instance_type          = var.instance_type
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   key_name               = aws_key_pair.thesis_key.key_name
