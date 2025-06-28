e3b02341541fe95e3e9775fb870f9d1ae1c552f0
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-28 20:42:01 +0200

    poprawka z ami linuxa

--- infra/ec2_pg_docker.tf
+++ infra/ec2_pg_docker.tf
@@ -1,14 +1,24 @@
-# infra/ec2_pg_docker.tf
+data "aws_ami" "amazonlinux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "pg_docker" {
-  ami = "ami-0df6cfabfbe438a58"  # Amazon Linux 2 dla eu-west-3
-  instance_type          = var.instance_type
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  key_name               = aws_key_pair.thesis_key.key_name
-  vpc_security_group_ids = [aws_security_group.thesis_sg.id]
-  availability_zone      = "eu-west-3a"
+  ami                         = data.aws_ami.amazonlinux2.id
+  instance_type               = var.instance_type
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  key_name                    = aws_key_pair.thesis_key.key_name
+  vpc_security_group_ids      = [aws_security_group.thesis_sg.id]
+  availability_zone           = "eu-west-3a"
 
   tags = {
-    Name = "postgres-docker"
+    Name = "postgres-native"
     Type = "db-test"
   }
 }
+
--- infra/ec2_pg_native.tf
+++ infra/ec2_pg_native.tf
@@ -11,19 +11,29 @@ data "aws_subnet" "default" {
   availability_zone = "eu-west-3a"   # Dostosuj jeśli chcesz
 }
 
-# 4. EC2 instance postgres
+data "aws_ami" "amazonlinux2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
 resource "aws_instance" "pg_native" {
-  ami = "ami-0df6cfabfbe438a58"  # Amazon Linux 2 dla eu-west-3
-  instance_type          = var.instance_type
-  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
-  key_name               = aws_key_pair.thesis_key.key_name
-  vpc_security_group_ids = [aws_security_group.thesis_sg.id]
-  availability_zone      = "eu-west-3a"
+  ami                         = data.aws_ami.amazonlinux2.id
+  instance_type               = var.instance_type
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  key_name                    = aws_key_pair.thesis_key.key_name
+  vpc_security_group_ids      = [aws_security_group.thesis_sg.id]
+  availability_zone           = "eu-west-3a"
 
   tags = {
     Name = "postgres-native"
     Type = "db-test"
   }
 }
+
 
 
