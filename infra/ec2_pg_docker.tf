0bf63338ada3a6ae5c81128cead7ed0da85a1076
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-28 20:37:53 +0200

    wracamy do linuxa

--- infra/ec2_pg_docker.tf
+++ infra/ec2_pg_docker.tf
@@ -1,5 +1,6 @@
+# infra/ec2_pg_docker.tf
 resource "aws_instance" "pg_docker" {
-  ami = "ami-01f89f442882cbed4"  # CentOS 7 (x86_64) dla eu-west-3
+  ami = "ami-0df6cfabfbe438a58"  # Amazon Linux 2 dla eu-west-3
   instance_type          = var.instance_type
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
   key_name               = aws_key_pair.thesis_key.key_name
