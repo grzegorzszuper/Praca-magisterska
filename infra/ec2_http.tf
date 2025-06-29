d15e729bc52b48542f1ab0e02dc73e0350dc0c8a
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-06-29 20:01:16 +0200

    Update ec2_http.tf

--- infra/ec2_http.tf
+++ infra/ec2_http.tf
@@ -1,5 +1,5 @@
 resource "aws_instance" "http_native" {
-  ami = data.aws_ami.amazonlinux2.id
+  ami = "ami-01f89f442882cbed4"
   instance_type          = var.instance_type
   key_name               = aws_key_pair.thesis_key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
@@ -14,7 +14,7 @@ resource "aws_instance" "http_native" {
 }
 
 resource "aws_instance" "http_docker" {
-  ami = data.aws_ami.amazonlinux2.id
+  ami = "ami-01f89f442882cbed4"
   instance_type          = var.instance_type
   key_name               = aws_key_pair.thesis_key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
@@ -29,7 +29,7 @@ resource "aws_instance" "http_docker" {
 }
 
 resource "aws_instance" "http_client" {
-  ami = data.aws_ami.amazonlinux2.id
+  ami = "ami-01f89f442882cbed4"
   instance_type          = var.instance_type
   key_name               = aws_key_pair.thesis_key.key_name
   iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
