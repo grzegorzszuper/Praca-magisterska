245adb49f391ad9157f70e4f55b961f62a925457
Grzegorz Szuper <grzegorzszuper2@gmail.com>
2025-07-01 17:23:06 +0200

    instancje io- native + docker

---
+++ infra/ec2_io.tf
@@ -0,0 +1,39 @@
+data "aws_ami" "amazonlinux2_stable" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-2.0.2023*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "io_native" {
+  ami = data.aws_ami.amazonlinux2_stable.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.thesis_key.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
+  vpc_security_group_ids = [aws_security_group.thesis_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "http-native"
+    Env  = var.env
+  }
+}
+
+resource "aws_instance" "io_docker" {
+  ami = data.aws_ami.amazonlinux2_stable.id
+  instance_type          = var.instance_type
+  key_name               = aws_key_pair.thesis_key.key_name
+  iam_instance_profile   = aws_iam_instance_profile.ec2_profile.name
+
+  vpc_security_group_ids = [aws_security_group.thesis_sg.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "http-docker"
+    Env  = var.env
+  }
+}
