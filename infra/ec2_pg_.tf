a913f2f9da91a5eef8f7666527a47b23eb474039
grzesiek <128179669+LyczekWody@users.noreply.github.com>
2025-07-08 20:00:34 +0200

    update pg

--- infra/ec2_pg_.tf
+++ infra/ec2_pg_.tf
@@ -22,3 +22,16 @@ resource "aws_instance" "pg_docker" {
   }
 }
 
+resource "aws_instance" "pg_native" {
+  ami                         = data.aws_ami.amazonlinux2.id
+  instance_type               = var.instance_type
+  iam_instance_profile        = aws_iam_instance_profile.ec2_profile.name
+  key_name                    = aws_key_pair.thesis_key.key_name
+  vpc_security_group_ids      = [aws_security_group.thesis_sg.id]
+  availability_zone           = "eu-west-3a"
+
+  tags = {
+    Name = "postgres-native"
+    Type = "db-test"
+  }
+}
