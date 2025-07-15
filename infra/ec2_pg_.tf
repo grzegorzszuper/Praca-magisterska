8e6ed3c3773e78b0b35e6d5072331f32f342f90b
Grzesiek <grzegorzszuper2@gmail.com>
2025-07-15 17:21:36 +0200

    Update ec2_pg_.tf

--- infra/ec2_pg_.tf
+++ infra/ec2_pg_.tf
@@ -1,12 +1,4 @@
-data "aws_ami" "amazonlinux2" {
-  most_recent = true
-  owners      = ["amazon"]
 
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-}
 
 resource "aws_instance" "pg_docker" {
   ami                         = data.aws_ami.amazonlinux2.id
